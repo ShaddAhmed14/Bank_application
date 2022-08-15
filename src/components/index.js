87eee9f06d6c5eed87456fe477dc45352136fcbe
ShaddAhmed14 <23100214@lums.edu.pk>
2022-08-15 16:49:12 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar"
+import Hero from "./Hero"
+import Billing from "./Billing"
+import Business from "./Business"
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import CTA from "./CTA"
+import Clients from "./Clients"
+import FeedbackCard from "./FeedbackCard"
+import GetStarted from "./GetStarted"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import Footer from "./Footer"
+
+export {
+    NavBar,
+    Hero,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    CTA,
+    Clients,
+    FeedbackCard,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Footer
+}
